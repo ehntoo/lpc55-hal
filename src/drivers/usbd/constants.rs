a1264ea77d873f00346d602fd3f43a6f3afef463
Mitchell Johnson <ehntoo@ehntoo.org>
2020-12-28 01:00:14 -0500

    Modify USB1_SRAM_ADDR for LPC551x parts

--- src/drivers/usbd/constants.rs
+++ src/drivers/usbd/constants.rs
@@ -7,7 +7,8 @@ pub type UsbAccessType = u8; // note this is just a type definition, we depend o
 pub const NUM_ENDPOINTS: usize = 1 + 5;
 pub const BYTES_PER_EP_REGISTER: usize = 4*4;
 
-pub const USB1_SRAM_ADDR: usize = 0x4010_0000;
+// pub const USB1_SRAM_ADDR: usize = 0x4010_0000;
+pub const USB1_SRAM_ADDR: usize = 0x2001_0000;
 pub const EP_MEM_ADDR: usize = USB1_SRAM_ADDR;
 pub const EP_MEM_SIZE: usize = 0x4000;
 pub const EP_REGISTERS_SIZE: usize = NUM_ENDPOINTS * BYTES_PER_EP_REGISTER;
